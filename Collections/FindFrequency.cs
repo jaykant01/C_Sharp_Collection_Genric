b62ed6e4568055f17e825dbdebe59bcaa796c090
jaykant01 <jaykanty24092004@gmail.com>
2026-01-15 14:42:33 +0530

    [Jaykant] Add .Find Frequency using Dictionary

---
+++ Collections/FindFrequency.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+
+namespace Collections;
+
+public class FindFrequency
+{
+    public void FindFreq()
+    {
+        List<string> list = new List<string>() { "apple", "banana", "apple", "orange" };
+        Dictionary<string, int> hm = new Dictionary<string, int>();
+
+        int count = 1;
+        for(int i=0; i<list.Count; i++)
+        {
+            if (hm.ContainsKey(list[i]))
+            {
+                hm[list[i]]++;
+            }
+            else
+            {
+                hm.Add(list[i], 1);
+            }
+        }
+
+        // print frequency
+        foreach(var item in hm)
+        {
+            Console.WriteLine(item.Key + " : " + item.Value);
+        }   
+    } 
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -8,34 +8,38 @@ class Program
         // List Interface Problems
 
         // Problem 1
-        ArrayList list = new ArrayList() { 1, 2, 3, 4, 5 };
+        // ArrayList list = new ArrayList() { 1, 2, 3, 4, 5 };
 
-        ReverseList reverseList = new ReverseList();
-        reverseList.Reverse(list);
+        // ReverseList reverseList = new ReverseList();
+        // reverseList.Reverse(list);
 
-        System.Console.WriteLine();
+        // System.Console.WriteLine();
 
-        LinkedList<int> linkedList = new LinkedList<int>();
-        linkedList.AddLast(1);
-        linkedList.AddLast(2);
-        linkedList.AddLast(3);
-        linkedList.AddLast(4);
-        linkedList.AddLast(5);
+        // LinkedList<int> linkedList = new LinkedList<int>();
+        // linkedList.AddLast(1);
+        // linkedList.AddLast(2);
+        // linkedList.AddLast(3);
+        // linkedList.AddLast(4);
+        // linkedList.AddLast(5);
 
-        Console.WriteLine("Original LinkedList:");
-        foreach (int item in linkedList)
-        {
-            Console.Write(item + " ");
-        }     
-        Console.WriteLine();
+        // Console.WriteLine("Original LinkedList:");
+        // foreach (int item in linkedList)
+        // {
+        //     Console.Write(item + " ");
+        // }     
+        // Console.WriteLine();
 
-        LinkedList<int> reversed = ReverseLinkedList.Reverse(linkedList);
+        // LinkedList<int> reversed = ReverseLinkedList.Reverse(linkedList);
 
-        Console.WriteLine("Reversed LinkedList:");
-        foreach (int item in reversed)
-        {
-            Console.Write(item + " ");
-        }
+        // Console.WriteLine("Reversed LinkedList:");
+        // foreach (int item in reversed)
+        // {
+        //     Console.Write(item + " ");
+        // }
+
+        // Problem 2
+        FindFrequency findFrequency = new FindFrequency();
+        findFrequency.FindFreq();
 
         Console.ReadKey();
                 
