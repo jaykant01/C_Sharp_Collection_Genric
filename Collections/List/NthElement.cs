a3b3821e7502ccebe02204e6b1a3dedc39f09ad1
jaykant01 <jaykanty24092004@gmail.com>
2026-01-15 15:48:26 +0530

    [Jaykant] Add .List All Questions

---
+++ Collections/List/NthElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Collections;
+
+public class NthElement
+{
+    public void Element()
+    {
+        LinkedList<char> ll = new LinkedList<char>();
+        ll.AddLast('A');
+        ll.AddLast('B');
+        ll.AddLast('C');
+        ll.AddLast('D');
+        ll.AddLast('E');
+
+        
+    }
+    
+    
+}
